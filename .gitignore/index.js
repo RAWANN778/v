f1ca0849889eaf9e19add4331772898f15bacef6
RAWANN778 <43817021+RAWANN778@users.noreply.github.com>
2018-10-03 15:55:29 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,107 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login("NDk2MDEzMjAzODYwMzU3MTIw.DpPb7A.OuC9fDzdm0nx5rvLUtlsoslmaw0")
+
+bot.on('message', message => {
+  if (message.content === "ping") {
+    message.reply("pong !")
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === "ah") {
+    message.reply("shtag xD!")
+  }
+}) 
+
+bot.on('message', message => {
+  if (message.content === "...") {
+   if (message.content === "zbeub") 
+    message.reply("Le rp c'est mieux !")
+    message.reply("franchement !")
+    message.reply("Choqué !")
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === "Caca") {
+    message.reply("._.")
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === "**µ") {
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+    message.reply("SPAMactivate")
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ;;;;
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === "REVELATION !!") {
+    message.reply(":open_mouth:")
+  }
+})
+
+bot.on("ready", function(){
+bot.user.setGame("Rawann le best (et aussi 尸乚凵爪巳尺工丹)")
+})
+
+bot.on('message', message => {
+  if (message.content === "BRUH!") {
+    message.reply(":open_mouth:")
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === "victime!") {
+    message.reply("BOLOSSE")
+  }
+})
